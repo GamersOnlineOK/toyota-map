d800f24fb6ce6e3f7a8c38edac0a8311ebf73245
Francisco Rangel <frankjrangel@gmail.com>
2021-09-29 23:45:46 -0300

    almost finished with about

--- src/components/aboutModal.js
+++ src/components/aboutModal.js
@@ -1,21 +1,78 @@
-/* eslint-disable react/prop-types */
+/* eslint-disable react/prop-types,max-len */
+
+import externalModuleSmallImg from '../assets/images/modulo-externo-small-about-modal.png';
+import externalModuleSmallP2Img from '../assets/images/modulo-externo-small-p2-about-modal.png';
+import devicePieces from '../assets/images/dispositivo-dislocado-about-modal.png';
+import carImage from '../assets/images/cuanto-tiempo-circula-hibrido-modo-electrico.png';
+import profilePicture from '../assets/images/profile-picture.png';
+
 export default function AboutModal(props) {
   const { active, toggle } = props;
 
   return (
-    <div id="about-modal" className={`modal modal-full-screen modal-fx-fadeInScale ${active ? 'is-active' : ''}`}>
+    <div id="modal-fadeInScale-fs" className={`modal modal-full-screen modal-fx-fadeInScale ${active ? 'is-active' : ''}`}>
       <div className="modal-content modal-card">
-        <header className="modal-card-head">
-          <p className="modal-card-title">Modal title</p>
-          <button type="button" className="modal-button-close delete" aria-label="close" onClick={toggle} />
-        </header>
-        <section className="modal-card-body">
-          ABOUT THE PROJECT
-        </section>
-        <footer className="modal-card-foot">
-          <button type="button" className="modal-button-close button is-success">Save changes</button>
-          <button type="button" className="modal-button-close button">Cancel</button>
-        </footer>
+        <div className="columns mt-6">
+          <h2 className="column is-four-fifths">ABOUT THE PROJECT</h2>
+          <div className="column is-one-fifths">
+            <button type="button" className="padded-close-button boxy-outline" aria-label="close" onClick={toggle}>
+              CLOSE
+            </button>
+          </div>
+        </div>
+        <div className="columns modal-container">
+          <div className="column is-two-fifths pr-6">
+            <p>El Proyecto Echo Index es un sistema de recolección de datos sobre diferentes aspectos relativos a la salud de los espacios y el impacto que puede tener en la vida de los seres vivos.</p>
+            <br />
+            <p>Las diferentes magnitudes relevadas son comunicadas a un servicio en la nube a través del smartphone de los colaboradores, donde son almacenados y preparados para el cálculo del índice Echo de calidad ambiental.</p>
+            <br />
+            <p>Dicho índice comprende desde magnitudes simples y perceptibles por una persona, como la temperatura ambiente e intensidad sonora, hasta parámetros más sutiles y solo perceptibles a través de instrumentos especiales, como el nivel CO2 junto con la cantidad de partículas y compuestos orgánicos en el aire.</p>
+          </div>
+          <div className="column is-two-fifths pr-6">
+            <p>Estos datos se envían de forma anónima y geolocalizada a la nube, conformando la arquitectura del sistema completo compuesto por varios sensores Echo en movimiento constante. Este movimiento permite generar un mapa dinámico de la calidad ambiental, de cada zona del país.</p>
+          </div>
+          <div className="column is-one-fifth">
+            <p><b>Modulo externo</b></p>
+            <p>Dispositivo integrado</p>
+            <img style={{ width: '237px', margin: '10px 0px' }} src={externalModuleSmallImg} alt="external module" />
+            <img style={{ width: '237px' }} src={externalModuleSmallP2Img} alt="external module part 2" />
+            <small>
+              <small>
+                Previsualizacion del dispositivo
+                <br />
+                {' '}
+                integrado en el auto
+              </small>
+            </small>
+          </div>
+        </div>
+        <p><b>Dispositivo dislocado</b></p>
+        <div className="columns">
+          <img src={devicePieces} alt="device pieces" />
+          Faltan los indicadores
+        </div>
+        <img style={{ minWidth: 'calc(100% + 300px)', marginLeft: '-150px' }} src={carImage} alt="car" />
+        <div className="section" />
+        <h3>About Curcio Capitals</h3>
+        <div style={{ marginTop: '20px' }} className="columns">
+          <div className="column is-three-fifths pr-6">
+            <p className="pr-6">
+              Echo Index es un proyecto impulsado por Curcio Capitals, apoyado sobre sus valores,
+              principios y creencias. Bajo los paraguas de “mejora contínua”, “respeto hacia
+              las personas” y “crecimiento sustentable”, la compañía persigue el ideal
+              de siempre procurar superarse a sí misma, cuidando a las personas y al Medio Ambiente.
+              Esta tarea la lleva a cabo mediante la introducción de tecnologías, productos y servicios
+              que contribuyan al bienestar ambiental y hagan más sencilla la vida de las personas en el mundo.
+            </p>
+            <br />
+            <br />
+            <br />
+            <b style={{ fontSize: '1.3rem' }}>Curcio Capitals, históricamente ha llevado adelante diferentes iniciativas que reflejan esta filosofía.</b>
+          </div>
+          <div className="column is-two-fifths">
+            <img style={{ width: '70%' }} src={profilePicture} alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
