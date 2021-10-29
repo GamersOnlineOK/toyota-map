56758df1d2ffbc188ffe22fd8eb72d59f753c8f2
Boreastudio <gutierrezfgraphicdesign@gmail.com>
2021-10-29 20:15:21 -0300

    hotfix #2

--- src/components/aboutModal.js
+++ src/components/aboutModal.js
@@ -26,7 +26,7 @@ export default function AboutModal(props) {
   }, [active]);
 
   return (
-    <div id="modal-fadeInScale-fs" className={`modal modal-full-screen modal-fx-fadeIn ${activeModal ? 'is-active' : ''}`}>
+    <div id="modal-fadeInScale-fs" className={`modal modal-full-screen fadeInScale ${activeModal ? 'is-active' : ''}`}>
       <div id="about-modal" className="modal-content modal-card">
         <div className="columns mt-6">
           <div className="modal-nav is-hidden-tablet">
