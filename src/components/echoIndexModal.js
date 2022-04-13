2bfc68aacf086754c40ab5f77643801655cd2048
Borea-Studio <103056525+Borea-Studio@users.noreply.github.com>
2022-04-13 19:13:30 -0300

    Update echoIndexModal.js

--- src/components/echoIndexModal.js
+++ src/components/echoIndexModal.js
@@ -100,9 +100,9 @@ export default function EchoIndexModal(props) {
           <div style={active ? { animationDuration: '4s' } : {}} className={`column opacity-0 is-one-third pl-0 ${active ? 'fade-in-delay-3' : ''}`}>
             <iframe width="100%" height="600" src="https://www.youtube.com/embed/bzCuN5SVMQ8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="" />
           </div>
-          <div>
-            <div className="columns modal-container" />
-          </div>
+          <button type="button" className="padded-close-button boxy-outline is-hidden-mobile" aria-label="close" onClick={toggle}>
+            CLOSE
+          </button>
         </div>
       </div>
     </div>
