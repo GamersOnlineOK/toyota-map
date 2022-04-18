ebb699cdacf8519e7ba43c36d641dfe2cd0084b8
Borea-Studio <103056525+Borea-Studio@users.noreply.github.com>
2022-04-18 12:11:57 -0300

    Testing Fix | Youtube related data is being cancelled by client

--- src/components/echoIndexModal.js
+++ src/components/echoIndexModal.js
@@ -98,7 +98,7 @@ export default function EchoIndexModal(props) {
             </ScrollAnimation>
           </div>
           <div style={active ? { animationDuration: '4s' } : {}} className={`column opacity-0 is-one-third pl-0 ${active ? 'fade-in-delay-3' : ''}`}>
-            <iframe width="100%" height="600" src="https://www.youtube.com/embed/bzCuN5SVMQ8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="" />
+            <iframe width="100%" height="600" src="https://www.youtube.com/embed/bzCuN5SVMQ8" frameBorder="0" allowFullScreen="" />
           </div>
           <button type="button" className="padded-close-button boxy-outline is-hidden-mobile" aria-label="close" onClick={toggle}>
             CLOSE
