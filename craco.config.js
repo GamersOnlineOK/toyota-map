37b6481b1ff1ab8253545d50aec897391f95cd5d
Leonardo <leo_dls@hotmail.com>
2022-04-27 15:07:31 +0000

    jquery added

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,13 @@
 module.exports = {
+  externals: {
+    // require(jquery) is external and available
+    //  on the global var jQuery
+    jquery: 'jQuery',
+  },
   babel: {
     loaderOptions: {
       ignore: ['./node_modules/mapbox-gl/dist/mapbox-gl.js'],
     },
   },
+
 };
