72ec2f9276e3267c10fc1d6598ac701523486f99
Borea-Studio <103056525+Borea-Studio@users.noreply.github.com>
2022-04-27 12:52:59 -0300

    Revert "jquery added"
    This reverts commit 37b6481b1ff1ab8253545d50aec897391f95cd5d.

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,7 @@
 module.exports = {
-  externals: {
-    // require(jquery) is external and available
-    //  on the global var jQuery
-    jquery: 'jQuery',
-  },
   babel: {
     loaderOptions: {
       ignore: ['./node_modules/mapbox-gl/dist/mapbox-gl.js'],
     },
   },
-
 };
