e5724bf37da358645a5c099708bbf36e62d79056
Leonardo De Los Santos <leo_dls@hotmail.com>
2022-05-02 21:31:55 -0300

    fix Utils.js map

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,25 +1,27 @@
+import WeeklyValuesLocal from '../assets/data/weekValues.json';
+
 export default function updateWeek(featureCollection, weeklyValues) {
   const { features } = featureCollection;
-  console.log(featureCollection);
-  console.log(weeklyValues);
-  return {
-    type: 'FeatureCollection',
-    features: features.map((f) => {
-      const zoneValue = weeklyValues.find((week) => week.zoneId === f.properties.zoneId);
-      console.log(zoneValue);
-      if (!zoneValue) {
-        return { ...f };
-      }
-
-      const values = { ...zoneValue };
-      delete values.week;
-      delete values.zoneId;
-
-      const properties = {
-        ...f.properties,
-        ...values,
-      };
-      return { ...f, properties };
-    }),
-  };
+  if (weeklyValues !== null) {
+    return {
+      type: 'FeatureCollection',
+      features: features.map((f) => {
+        const zoneValue = weeklyValues.find((week) => week.zoneId === f.properties.zoneId);
+        if (!zoneValue) {
+          return { ...f };
+        }
+        const values = { ...zoneValue };
+        delete values.week;
+        delete values.zoneId;
+        const properties = {
+          ...f.properties,
+          ...values,
+        };
+        return { ...f, properties };
+      }),
+    };
+    // eslint-disable-next-line
+  } else {
+    return { WeeklyValuesLocal };
+  }
 }
