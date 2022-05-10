9952c9b9e22ccc540bedf776aaef6e8aab41ea90
Borea-Studio <103056525+Borea-Studio@users.noreply.github.com>
2022-05-10 10:25:10 -0300

    Update IntroPage.js

--- src/pages/IntroPage.js
+++ src/pages/IntroPage.js
@@ -95,6 +95,7 @@ export default function IntroPage() {
         <Lottie
           options={echoLottieOptions}
           loop={false}
+          autoplay={true}
           animationData={echoAnimation}
           rendererSettings={{ preserveAspectRatio: 'xMidYMid slice' }}
           height={50}
